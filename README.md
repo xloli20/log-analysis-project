83904bf558dba093618ceb67f2414f54a2a7e6bf
Alaa Alzahrani <32674646+xloli20@users.noreply.github.com>
2018-11-10 15:36:18 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# log-analysis-project
+# Log Analysis project
+
+First project in Udacity Fullstack Nanodegree
+It's a reporting tool that prints out reports (in plain text) based on the data in the database. This reporting tool is a Python program using the psycopg2 module to connect to the database.
+
+Answering the flowing question:
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of requests lead to errors?
+
+# Installation
+
+  - [Python](https://www.python.org/downloads/)
+  - [Vagrant](https://www.vagrantup.com/downloads.html)
+  - [Virtual Machine](https://www.virtualbox.org/wiki/Downloads)
+  - [The Database](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+
+## How to Run the project?
+Once you get the above installed, run the flowing in the terminal:
+
+you should downlad [FSND virtual machine](https://github.com/udacity/fullstack-nanodegree-vm). then,
+```sh 
+$ cd vagrant
+```
+to Launch Vagrant VM
+```sh 
+$ vagrant up
+```
+
+to login into the VM
+```sh 
+$ vagrant ssh
+```
+
+then,
+```sh 
+$ cd /vagrant
+```
+you should downlaod or clone the project inside the vagrant folder. then run
+```sh 
+$ cd log-analysis-project
+```
+
+ and finally to run the program and print the results of the three question, run
+ ```sh 
+$ python log-analyser.py
+```
+
---
+++ log-analyser.py
@@ -0,0 +1,63 @@
+import psycopg2
+
+
+class Questions:
+    dbname = "news"
+
+    def __init__(self):
+        try:
+            self.db = psycopg2.connect(database=self.dbname)
+            self.cursor = self.db.cursor()
+        except:
+            return 'can not connect to the database'
+
+    def popularArticle(self):
+        print('What are the most popular three articles of all time?')
+        self.cursor.execute("""select title, count(*) as views
+            from articles inner join log
+            on log.path like concat('/article/', articles.slug, '%')
+            group by log.path, articles.title
+            order by views desc limit 3;""")
+        result = self.cursor.fetchall()
+        for e, e2 in result:
+            print '.', e, '---', e2, 'views'
+
+    def popularAuthors(self):
+        print('Who are the most popular article authors of all time?')
+        self.cursor.execute("""select authors.name, count(*) as views
+            from articles inner join authors
+            on articles.author = authors.id inner join log
+            on log.path like concat('/article/', articles.slug, '%')
+            group by authors.name
+            order by views desc;""")
+        result = self.cursor.fetchall()
+        for e, e2 in result:
+            print '.', e, '---', e2, 'views'
+
+    def error(self):
+        print('On which days did more than 1% of requests lead to errors?')
+        self.cursor.execute("""select day, percent from (
+           select day, round((sum(errorReqs)/
+           (select count(*) from log where date("time") = day)
+           * 100), 2) as percent from
+           (select date("time") as day,
+           count(*) as errorReqs from log
+           where status = '404 NOT FOUND' group by day)
+           as logPercentage group by day order by percent desc) as finalQ
+           where percent >= 1.0""")
+        result = self.cursor.fetchall()
+        for e, e2 in result:
+            print '.', e, '---', e2, '% errors'
+
+    def destroyConnection(self):
+        self.db.close()
+
+if __name__ == '__main__':
+    Questions = Questions()
+    Questions.popularArticle()
+    print ''
+    Questions.popularAuthors()
+    print ''
+    Questions.error()
+    print ''
+    Questions.destroyConnection()
